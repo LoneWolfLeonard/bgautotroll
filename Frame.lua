98c7742facd36344081cb200c0c1564b2b7ed7ac
Leonard Treman <leonardtreman@gmail.com>
2018-01-09 23:33:07 -0500

    Add files via upload

---
+++ Frame.lua
@@ -0,0 +1,9 @@
+﻿local BgTroll = CreateFrame("Frame")
+
+BgTroll:RegisterEvent("GROUP_ROSTER_UPDATE")
+
+BgTroll:SetScript("OnEvent", function(Self, event, ...)
+
+if event == "GROUP_ROSTER_UPDATE" then 
+SendChatMessage("Has Reported You Afk, Type, \"/Afk Report\" To Rid Yourself Of This Status Before You Are Removed From The Battleground.", "EMOTE", "nil", "index");end
+end)
